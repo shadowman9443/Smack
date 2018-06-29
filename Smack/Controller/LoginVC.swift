180022891f07d4d6994eb8f34b63aa8c93a96737
aunogarafat <shadowman9443@gmail.com>
2018-06-29 10:44:46 +0600

    Complete the design of LodinVC & connect with ChannelVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by aunogarafat on 6/29/18.
+//  Copyright © 2018 aunogarafat. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginDismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by aunogarafat on 6/29/18.
+//  Copyright © 2018 aunogarafat. All rights reserved.
+//
+
+import Foundation
+let TO_LOGIN = "toLogin"
